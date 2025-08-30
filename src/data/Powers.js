2864db74bbd9b51b1f14af1f3b1d618bb1e2a5cd
Martin <mc55jr@gmail.com>
2025-08-30 01:42:18 -0700

    fixed imgs so they come up

--- src/data/Powers.js
+++ src/data/Powers.js
@@ -1,18 +1,23 @@
+// src/data/Powers.js
+import airIcon from '../assets/img/air-icon.jpg';
+import waterIcon from '../assets/img/water-icon.png';
+import fireIcon from '../assets/img/fire-icon.png';
+
 const powers = [
   {
     id: 1,
     name: "air",
-    img: "/img/air-icon.jpg"
+    img: airIcon
   },
   {
     id: 2,
     name: "water",
-    img: "/img/water-icon.png"
+    img: waterIcon
   },
   {
     id: 3,
     name: "fire",
-    img:"/img/fire-icon.png"
+    img: fireIcon
   },
 ];
 
